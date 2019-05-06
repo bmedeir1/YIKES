0d4ffb5038055d90e8a750c73b9a989c301ee085
Beatriz Medeiros <bmedeir1@jhu.edu>
2019-05-06 18:00:24 -0400

    Got Bluetooth to work

--- app/src/main/java/com/example/yikes/Bluetooth/MyBluetoothService.java
+++ app/src/main/java/com/example/yikes/Bluetooth/MyBluetoothService.java
@@ -1,7 +1,10 @@
 package com.example.yikes.Bluetooth;
 
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothSocket.*;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -10,6 +13,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.UUID;
 
 import static android.content.ContentValues.TAG;
 
@@ -17,15 +21,91 @@ import static android.content.ContentValues.TAG;
 public class MyBluetoothService {
     private static final String TAG = "MY_APP_DEBUG_TAG";
     private Handler handler; // handler that gets info from Bluetooth service
+    private BluetoothDevice mmBluetoothDevice;
 
     // Defines several constants used when transmitting messages between the
     // service and the UI.
+
+    public MyBluetoothService(BluetoothDevice device, Handler handler) {
+        Log.d(TAG, "MyBluetoothService constructor");
+        this.mmBluetoothDevice = device;
+        this.handler = handler;
+        ConnectThread mmConnectThread = new ConnectThread(this.mmBluetoothDevice);
+        mmConnectThread.start();
+    }
+
+
     private interface MessageConstants {
         public static final int MESSAGE_READ = 0;
         public static final int MESSAGE_WRITE = 1;
         public static final int MESSAGE_TOAST = 2;
 
         // ... (Add other message types here as needed.)
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();;
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //standard SPP for Bluetooth serial board
+
+        public ConnectThread(BluetoothDevice device) {
+            Log.d(TAG, "ConnectThread constructor");
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                Log.d(TAG, " At createRfcommSocketToServiceRecord");
+//                tmp = createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            Log.i(TAG, "Runing run() of Connect Thread");
+            // Cancel discovery because it otherwise slows down the connection.
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+                Log.d(TAG, " At mmSocket.connect()");
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                    Log.d(TAG, " At mmSocket.close()");
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            ConnectedThread mmConnectedThread = new ConnectedThread(mmSocket);
+            mmConnectedThread.start();
+
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+
+
     }
 
     private class ConnectedThread extends Thread {
@@ -35,6 +115,7 @@ public class MyBluetoothService {
         private byte[] mmBuffer; // mmBuffer store for the stream
 
         public ConnectedThread(BluetoothSocket socket) {
+            Log.d(TAG, "ConnectedThread constructor");
             mmSocket = socket;
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
@@ -57,7 +138,7 @@ public class MyBluetoothService {
         }
 
         public void run() {
-            Log.i(TAG, "Runing run() of Bluetooth Service");
+            Log.d(TAG, "Runing run() of Bluetooth Service");
             mmBuffer = new byte[1024];
             int numBytes; // bytes returned from read()
 
@@ -111,10 +192,5 @@ public class MyBluetoothService {
         }
     }
 
-    public void manageMyConnectedSocket(BluetoothSocket mmSocket) {
-        ConnectedThread mmConnectedThread = new ConnectedThread(mmSocket);
-        mmConnectedThread.run();
 
-
-    }
 }
--- app/src/main/java/com/example/yikes/MainActivity.java
+++ app/src/main/java/com/example/yikes/MainActivity.java
@@ -3,49 +3,66 @@ package com.example.yikes;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.yikes.Bluetooth.ConnectThread;
+
+import com.example.yikes.Bluetooth.MyBluetoothService;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     BluetoothAdapter bluetoothAdapter;
+    Handler handler;
+    TextView connect_status;
+    TextView value;
+    TextView switch
+    BluetoothDevice mmBluetoothDevice = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connect_status = findViewById(R.id.connect_status);
+        value = findViewById(R.id.value);
+
         Boolean blt_supported = true;
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
-            System.out.println("Couldn't establish bluetooth connection");
+            Log.d("TAG", "Couldn't establish bluetooth connection");
             blt_supported = false;
         }
 
+        Log.d("TAG", "here 1");
         int REQUEST_ENABLE_BT = 1;
 //        int attempts = 0;
-        while (blt_supported && REQUEST_ENABLE_BT != RESULT_OK) {
+//        while (blt_supported && REQUEST_ENABLE_BT != RESULT_OK) {
             if (!bluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 
             }
 //            ++attempts;
-        }
+//        }
 
 //        if (attempts >= 5) {
 //            System.out.println("Couldn't enable bluetooth");
 //        }
 
         String deviceAddress;
-        BluetoothDevice blt_device = null;
 
-        if (blt_supported && REQUEST_ENABLE_BT == RESULT_OK) {
+        Log.d("TAG", "HERE 2");
+        if (bluetoothAdapter.isEnabled()) {
+            Log.d("TAG", "HERE 3");
             Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
             String deviceName = "HC-05";
 
@@ -53,10 +70,10 @@ public class MainActivity extends AppCompatActivity {
             if (pairedDevices.size() > 0) {
                 // There are paired devices. Get the name and address of each paired device.
                 for (BluetoothDevice device : pairedDevices) {
-//                    String deviceName = device.getName();
-                    if (device.getName() == deviceName) {
+                    Log.i("TAG", "Device name " + device.getName());
+                    if (device.getName().equals(deviceName)) {
                         deviceAddress = device.getAddress(); // MAC address
-                        blt_device =  device;
+                        mmBluetoothDevice =  device;
                         break;
                     }
                 }
@@ -68,9 +85,21 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        if (blt_device != null) {
-            ConnectThread mmConnectThread = new ConnectThread(blt_device);
-            mmConnectThread.run();
+        Log.i("TAG", "Selected Device name " + mmBluetoothDevice.getName());
+
+        if (mmBluetoothDevice != null) {
+//            Looper.getMainLooper();
+//            Looper.prepare();
+            handler = new Handler() {
+
+                @Override
+                public void handleMessage(Message msg) {
+                    value.setText(msg.toString());
+                    connect_status.setText("Connected");
+
+                }
+            };
+            MyBluetoothService mmMyBluetoothService = new MyBluetoothService(mmBluetoothDevice, handler);
 
         }
 
